8e31c1beffcb6d47a78434f0a7ea0289cb29538b
Tracy Ngo <tngo1206@gmail.com>
2019-08-10 17:33:39 -0700

    Done.

---
+++ db/migrate/20190811002134_create_preferences.rb
@@ -0,0 +1,10 @@
+class CreatePreferences < ActiveRecord::Migration[5.0]
+  def change
+    create_table :preferences do |t|
+      t.string :song_sort_order
+      t.string :artist_sort_order
+      t.boolean :allow_create_artists
+      t.boolean :allow_create_songs
+    end
+  end
+end
